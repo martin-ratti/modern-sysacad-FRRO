64b2f2df666f3a1a6b0f0c62d1f7b19a3e8c419b
tomaskoblukUTN <tomas.kobluk@ca.frre.utn.edu.ar>
2026-05-27 14:45:43 -0300

    Limpiar background.js no utilizado y configs de editor

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
-    if (msg.type === 'openGCalTabs' && Array.isArray(msg.urls)) {
-        msg.urls.forEach((url, i) => {
-            chrome.tabs.create({ url, active: i === 0 });
-        });
-        sendResponse({ ok: true });
-    }
-    return true;
-});
